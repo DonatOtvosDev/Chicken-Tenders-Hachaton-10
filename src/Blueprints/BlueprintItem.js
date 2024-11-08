da677f1f6aaa5499a0ef0d43318557af45d846fd
DonatOtvosDev <donatotvosofficial@gmail.com>
2024-11-08 12:17:26 +0100

    adding blueprints

---
+++ src/Blueprints/BlueprintItem.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+import { useTheme } from "../ThemeProvider";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+
+function BlueprintItem({ data }) {
+  const { theme } = useTheme();
+
+  const styles = StyleSheet.create({
+    card: {
+      width: "90%",
+      borderRadius: 20,
+      shadowColor: "#000",
+      shadowOpacity: 0.3,
+      backgroundColor: "#fff",
+      shadowRadius: 4,
+      elevation: 4,
+      margin: 8,
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: "700",
+      textAlign: "left",
+    },
+    body: {
+      fontSize: 16,
+      textAlign: "justify",
+      flex: 1,
+    },
+    image: {
+      width: "100",
+      height: 200,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+    },
+    contentContainer: {
+      padding: 10,
+    },
+    row: {
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    listConatiner: {
+      flex: 1,
+    },
+    dot: {
+      width: 6,
+      height: 6,
+      borderRadius: 3, // Circular dot
+      backgroundColor: "#000", // Dot color
+      marginRight: 10,
+    },
+    itemText: {
+      fontSize: 16,
+    },
+    listItem: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 5,
+    },
+  });
+
+  return (
+    <View style={styles.card}>
+      <Image style={styles.image} source={{ uri: data["image_url"] }} />
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{data["name"]}</Text>
+        <View style={styles.row}>
+          <Text style={styles.body}>{data["description"]}</Text>
+          <View style={styles.listConatiner}>
+            <FlatList
+              data={data["elements"]}
+              renderItem={({ item }) => {
+                return (
+                  <View style={styles.listItem}>
+                    <View style={styles.dot} />
+                    <Text style={styles.itemText}>{item}</Text>
+                  </View>
+                );
+              }}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default BlueprintItem;
--- src/Blueprints/BlueprintScreen.js
+++ src/Blueprints/BlueprintScreen.js
@@ -1,31 +1,64 @@
-import React, {useState} from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import BlueprintItem from "./BlueprintItem";
 
 import { useTheme } from "../ThemeProvider";
 
 function BlueprintScreen() {
-    const blueprints = [
-        {
-            "name" : "Truck",
-            "description" : "this is a beautiful",
-            "components" : ["drill", "wheel", "rubber"]
-        }
-   ]
+  const blueprints = [
+    {
+      name: "Truck",
+      description: "this is a beautiful",
+      image_url:
+        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+      elements: ["tire", "wheel", "box"],
+    },
+    {
+      name: "Truck",
+      description: "this is a beautiful",
+      image_url:
+        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+      elements: ["tire", "wheel", "box"],
+    },
+    {
+      name: "Truck",
+      description: "this is a beautiful",
+      image_url:
+        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+      elements: ["tire", "wheel", "box"],
+    },
+  ];
 
-   const { theme } = useTheme();
+  const { theme } = useTheme();
 
-   const styles = StyleSheet.create({
-        titleText: {
-            fontSize: 20
-        }
-   })
+  const styles = StyleSheet.create({
+    titleText: {
+      fontSize: 20,
+    },
+    listConatiner: {
+      backgroundColor: "#333",
+      justifyContent: "center",
+      flex: 1,
+    },
+    backGound: {
+      flex: 1,
+      alignContent: "center",
+    },
+  });
 
-   return (
-    <SafeAreaView>
-        <Text style={styles.titleText}>Blueprints</Text>
-        <View></View>
+  return (
+    <SafeAreaView style={styles.backGound}>
+      <Text style={styles.titleText}>Blueprints</Text>
+
+      <View style={styles.listConatiner}>
+        <FlatList
+       contentContainerStyle={{ alignItems: "center" }}
+          data={blueprints}
+          renderItem={({ item }) => <BlueprintItem data={item} />}
+        />
+      </View>
     </SafeAreaView>
-   )
+  );
 }
 
 export default BlueprintScreen;
